ebf5e8c2c2f5daeece5708c29c61829421abad79
Louis Coquio <louis.coquio@gmail.com>
2015-02-10 18:08:52 +0100

    Entry#startedAt and #finishedAt with HH:mm format

--- app/controllers/entry.js
+++ app/controllers/entry.js
@@ -1,8 +1,33 @@
 import Ember from 'ember';
+import moment from 'moment';
+
+function updateHoursAndMinutes(date, hours, minutes) {
+  var newDate = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    hours,
+    minutes
+  );
+  if (!isNaN(newDate.getTime())) {
+    return newDate;
+  }
+}
 
 export default Ember.ObjectController.extend({
   needs: 'index',
   isEditing: null,
+
+  startedAtHour: function() {
+    return moment(this.get('startedAt')).format('H:mm');
+  }.property('startedAt'),
+
+  finishedAtHour: function() {
+    return moment(this.get('finishedAt')).format('H:mm');
+  }.property('finishedAt'),
+
+  startedAtHourOneWayBinding: Ember.Binding.oneWay('startedAtHour'),
+  finishedAtHourOneWayBinding: Ember.Binding.oneWay('finishedAtHour'),
 
   actions: {
     editEntry: function() {
@@ -21,6 +46,16 @@ export default Ember.ObjectController.extend({
       this.get('content').destroyRecord().then(function() {
         index.send('refresh');
       });
+    },
+    startedAtHourChanged: function(string) {
+      var split = string.split(':');
+      var newValue = updateHoursAndMinutes(this.get('startedAt'), split[0], split[1]);
+      this.get('content').set('startedAt', newValue);
+    },
+    finishedAtHourChanged: function(string) {
+      var split = string.split(':');
+      var newValue = updateHoursAndMinutes(this.get('finishedAt'), split[0], split[1]);
+      this.get('content').set('finishedAt', newValue);
     }
   }
 });
