038c59410f05c6c289dbd0b3a0f214dc516c7c96
Louis Coquio <louis.coquio@gmail.com>
2015-04-19 01:46:34 +0200

    Delete an entry removes it from the database

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope '/api' do
-    resources :entries, only: [:create, :update, :index]
+    resources :entries, only: [:create, :update, :destroy, :index]
     resources :projects, only: [:index]
   end
 end
