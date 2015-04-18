4f976a72807b18b964a91aaf05498d4a17e4356c
Louis Coquio <louis.coquio@gmail.com>
2015-04-18 22:56:41 +0200

    Test setup

--- rails/test/test_helper.rb
+++ rails/test/test_helper.rb
@@ -1,7 +1,27 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'minitest/rails'
+
+module TestCaseExtension
+  extend ActiveSupport::Concern
+
+  included do
+    include FactoryGirl::Syntax::Methods
+    before do
+      DatabaseCleaner.start
+    end
+    after do
+      DatabaseCleaner.clean
+    end
+  end
+end
+
+class ::Minitest::Spec
+  include TestCaseExtension
+end
 class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
+  include TestCaseExtension
 end
