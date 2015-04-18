8b810be308a69d274bf05da82d6c4611b8872dd3
Louis Coquio <louis.coquio@gmail.com>
2015-04-19 01:14:12 +0200

    Create an entry saves it into the database

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,11 @@
 class EntriesController < ApplicationController
 
+  def create
+    entry = Entry.new(entry_params)
+    entry.save!
+    render json: { entry: serialize_entry(entry) }
+  end
+
   def index
     entries = Entry.all
     projects = entries_projects(entries)
@@ -12,6 +18,10 @@ class EntriesController < ApplicationController
 
   private
 
+  def entry_params
+    params.require(:entry).permit(:title, :started_at, :finished_at)
+  end
+
   def entries_projects(entries)
     entries.each_with_object(Set.new) do |entry, set|
       set << entry.project if entry.project
@@ -19,15 +29,17 @@ class EntriesController < ApplicationController
   end
 
   def serialize_entries(entries)
-    entries.map do |entry|
-      {
-        id: entry.id,
-        title: entry.title,
-        startedAt: entry.started_at,
-        finishedAt: entry.finished_at,
-        project: entry.project_id
-      }
-    end
+    entries.map { |entry| serialize_entry(entry) }
+  end
+
+  def serialize_entry(entry)
+    {
+      id: entry.id,
+      title: entry.title,
+      startedAt: entry.started_at,
+      finishedAt: entry.finished_at,
+      project: entry.project_id
+    }
   end
 
   def serialize_projects(projects)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope '/api' do
-    resources :entries, only: [:index]
+    resources :entries, only: [:create, :index]
     resources :projects, only: [:index]
   end
 end
