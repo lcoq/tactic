2e357053a054b293ddafce2f78c8eea131805448
Louis Coquio <louis.coquio@gmail.com>
2015-04-29 18:35:01 +0200

    Current user week hours

--- ember/app/controllers/application.js
+++ ember/app/controllers/application.js
@@ -11,6 +11,23 @@ function isInCurrentWeek(entry, startOfWeek) {
   return (date.getTime() - startOfWeek.getTime()) >= 0;
 }
 
+function belongsTo(entry, user) {
+  return entry.get('user.id') === user.id;
+}
+
+function projectsDuration(entries) {
+  var projects = entries.mapProperty('project').uniq(), projectEntries, duration;
+  return projects.map(function(project) {
+    projectEntries = entries.filterProperty('project.id', project ? project.get('id') : null);
+    duration = projectEntries.reduce(function(s, entry) { return s + entry.get('duration'); }, 0);
+    return Ember.Object.create({ project: project, duration: duration });
+  }).sortBy('duration').reverse();
+}
+
+function totalProjectsDuration(durations) {
+  return durations.reduce(function(s, projectDuration) { return s + projectDuration.get('duration'); }, 0);
+}
+
 export default Ember.Controller.extend({
   needs: 'index',
 
@@ -24,22 +41,25 @@ export default Ember.Controller.extend({
   }.property(),
 
   projectsDurationInCurrentWeek: function() {
-    var entries = this.get('entriesInCurrentWeek');
-    var projects = entries.mapProperty('project').uniq();
-    var projectsDuration = projects.map(function(project) {
-      var projectId = project ? project.get('id') : null;
-      var duration = entries.filterProperty('project.id', projectId).reduce(function(previous, entry) {
-        return previous + entry.get('duration');
-      }, 0);
-      return Ember.Object.create({ project: project, duration: duration });
-    });
-    return projectsDuration.sortBy('duration').reverse();
+    return projectsDuration(this.get('entriesInCurrentWeek'));
   }.property('entriesInCurrentWeek.@each.duration', 'entriesInCurrentWeek.@each.project'),
 
   totalDurationInCurrentWeek: function() {
-    var totalDuration = this.get('projectsDurationInCurrentWeek').reduce(function(previous, projectDuration) {
-      return previous + projectDuration.get('duration');
-    }, 0);
-    return totalDuration;
-  }.property('projectsDurationInCurrentWeek.@each.duration')
+    return totalProjectsDuration(this.get('projectsDurationInCurrentWeek'));
+  }.property('projectsDurationInCurrentWeek.@each.duration'),
+
+  myEntriesInCurrentWeek: function() {
+    var startOfCurrentWeek = moment().startOf('isoWeek').toDate(), user = this.get('currentUser');
+    return this.store.filter('entry', function(entry) {
+      return isFilled(entry) && belongsTo(entry, user) && isInCurrentWeek(entry, startOfCurrentWeek);
+    });
+  }.property('currentUser'),
+
+  myProjectsDurationInCurrentWeek: function() {
+    return projectsDuration(this.get('myEntriesInCurrentWeek'));
+  }.property('myEntriesInCurrentWeek.@each.duration', 'myEntriesInCurrentWeek.@each.project'),
+
+  myTotalDurationInCurrentWeek: function() {
+    return totalProjectsDuration(this.get('myProjectsDurationInCurrentWeek'));
+  }.property('myProjectsDurationInCurrentWeek.@each.duration')
 });
